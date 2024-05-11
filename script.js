e5b77b5f3a6403c4efa53478f60d25484ca70679
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-12 04:18:10 +0500

    Fixed Border issue

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 function generateRandomRGBNumber() {
-    return Math.floor(Math.random() * 256)
+    return Math.floor(Math.random() * 256);
 }
 
 function generateRandomRGBColor() {
-    return `${generateRandomRGBNumber()},${generateRandomRGBNumber()},${generateRandomRGBNumber()}`
+    return `${generateRandomRGBNumber()},${generateRandomRGBNumber()},${generateRandomRGBNumber()}`;
 }
 
-function createDiv(number = 1){
-    layoutWidth = number*18
-    const divList = []
-    for (let i = 1; i <= number*number; i++){
-        const container = document.querySelector(".container")
-        const div = document.createElement("div")
-        divList[i] = div
-        div.setAttribute('class', 'box')
-        container.style.width = `${layoutWidth}px`
-        div.addEventListener('mouseover', (event) => {
+function createDiv(number = 1) {
+    layoutWidth = number * 7;
+    const divList = [];
+    for (let i = 1; i <= number * number; i++) {
+        const container = document.querySelector(".container");
+        const div = document.createElement("div");
+        divList[i] = div;
+        div.setAttribute('class', 'box');
+        container.style.width = `${layoutWidth}px`;
+        div.addEventListener('mouseover', (event) => {  
             div.style.backgroundColor = "black";
-            event.stopPropagation()
-        })
-        container.appendChild(div)
+            div.setAttribute('class', 'box marked');
+            if (bordersButton.textContent == "Grid Lines"){
+                div.style.border = "1px solid black"
+            }
+            markedDivs.push(div);
+            event.stopPropagation();
+        });
+        container.appendChild(div);
     }
-    return divList
+    return divList;
 }
 
-x = createDiv(30)
-const clearButton = document.querySelector("#clear")
+let markedDivs = [];
+const x = createDiv(100);
+const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', () => {
     x.forEach(element => {
-        element.style.backgroundColor = "white"
+        element.style.backgroundColor = "white";
     });
-})
-const bordersButton = document.querySelector("#remborders")
+    markedDivs = [];
+});
+
+// Getting the Border Button
+const bordersButton = document.querySelector("#remborders");
+
+// Adding Event Listener to switch between No Borders and Gridlines
 bordersButton.addEventListener('click', () => {
-    x.forEach(element => {
-        element.style.border = "1px solid white"
-    });
-})
+    // Changing the borders to white
+    if (bordersButton.textContent == "Remove Borders") {
+        x.forEach((element, index) => {
+            if (!markedDivs.includes(x[index])) {
+                element.style.border = "1px solid white";
+            }
+        });
+        bordersButton.textContent = "Grid Lines";
+    } else { // Changing the borders back to black
+        x.forEach(element => {
+            element.style.border = "1px solid black";
+        });
+        bordersButton.textContent = "Remove Borders";
+    }
+});
+
+const testButton = document.querySelector('#test');
+testButton.addEventListener('click', () => console.log(markedDivs));
